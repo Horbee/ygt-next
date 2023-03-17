7ef640ab15f4b4f48e18d3f7d7310ae11a32e71c
Norbert Horgas <norbert.horgas@externals.adidas.com>
2023-03-17 22:38:41 +0100

    Don't reset scroll position when selecting availability date

--- src/hooks/useSelectedDate.ts
+++ src/hooks/useSelectedDate.ts
@@ -17,7 +17,9 @@ export const useSelectedDate = () => {
     if (date) params.set("date", getFormattedDate(date));
     else params.delete("date");
 
-    router.push(pathname + "?" + params.toString());
+    router.push({ pathname: pathname, query: params.toString() }, undefined, {
+      scroll: false,
+    });
   };
 
   return { selectedDate, setSelectedDate };
